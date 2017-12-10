d6734c9ba85544539fecd5e625a4363abfd5f2a8
Reginaldo Alves de Barros <R21Regis@hotmail.com>
2017-12-10 19:18:38 -0200

    estrutura do menu

---
+++ src/GoodRecipe.UWP/Views/MainView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace GoodRecipe.UWP.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainView : Page
+    {
+        public MainView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void MenuGrid_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (NavigationPane.IsPaneOpen)
+                NavigationPane.IsPaneOpen = !NavigationPane.IsPaneOpen;
+
+            //MenuItem menu = LeftMenu.SelectedItem as MenuItem;
+            //if (menu != null)
+            //{
+            //    if (menu.NavigateTo != null)
+            //        CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => { FrameContent.Navigate(menu.NavigateTo); });
+            //}
+        }
+    }
+}
